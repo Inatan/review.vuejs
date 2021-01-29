f1aa317b3706bc0a912e7b77f343dcfc769a697d
inatan <inatan_hertzog@hotmail.com>
2021-01-28 22:51:19 -0300

    finishing learning vue

--- vueProj/vueTstPic/src/components/home/Home.vue
+++ vueProj/vueTstPic/src/components/home/Home.vue
@@ -1,11 +1,19 @@
 <template>
   <div>
     <h1 class="centralizado">{{titulo}}</h1>
+
+    <p v-show="mensagem" class="centralizado">{{ mensagem }}</p>
       <input type="search" class="filtro" @input="filtro = $event.target.value" placeholder="filtre pelo título da foto">
       <ul class="lista-fotos">
         <li class="lista-fotos-item" v-for="foto of fotosComFiltro" :key="foto.titulo">
           <meu-painel :titulo="foto.titulo">
-            <imagem-responsiva :url="foto.url" :titulo="foto.titulo"/>
+            <imagem-responsiva :url="foto.url" :titulo="foto.titulo" v-meu-transform="{incremento:45, animate: true}"/>
+              <router-link :to="{ name: 'altera', params: { id : foto._id }}">
+                <meu-botao 
+                  rotulo="Alterar" 
+                  tipo="button"/>
+              </router-link>  
+            
             <meu-botao 
               rotulo="remover" 
               tipo="button" 
@@ -23,6 +31,7 @@
 import Painel from '../shared/painel/Painel.vue';
 import ImagemResponsiva from '../shared/imagem-responsiva/ImagemResponsiva.vue';
 import Botao from '../shared/botao/Botao.vue';
+import FotoService from '../../domain/foto/FotoService';
 
 export default {
   components:{
@@ -42,7 +51,17 @@ export default {
   methods: {
 
     remove(foto) {
-      alert(foto.titulo);
+
+      this.service
+        .apaga(foto._id)
+        .then(
+          () => {
+            let indice = this.fotos.indexOf(foto);
+            this.fotos.splice(indice, 1);
+            this.mensagem = 'Foto removida com sucesso'
+          }, 
+          err => this.mensagem = err.message
+        )
     }
   },
 
@@ -57,10 +76,12 @@ export default {
     }
   },
 
-  created(){
-    this.$http.get("http://localhost:3000/v1/fotos")
-      .then(res => res.json())
-      .then(fotos => this.fotos = fotos);
+  created() {
+
+    this.service = new FotoService(this.$resource);
+
+    this.service.lista()
+      .then(fotos => this.fotos = fotos, err => this.mensagem = err.message);
   }
 }
 </script>
--- vueProj/vueTstPic/src/routes.js
+++ vueProj/vueTstPic/src/routes.js
@@ -2,6 +2,8 @@ import Home from './components/home/Home.vue';
 import Cadastro from './components/cadastro/Cadastro.vue';
 
 export const routes = [
-    { titulo: 'home', path: '', component: Home},
-    { titulo: "cadastro", path: '/cadastro', component: Cadastro},
+    { path: '', name: 'home', component: Home, titulo: 'Home', menu: true },
+    { path: '/cadastro', name:'cadastro', component: Cadastro, titulo: 'Cadastro', menu: true },
+    { path: '/cadastro/:id', name:'altera', component: Cadastro, titulo: 'Cadastro', menu: false },    
+    { path: '*', component: Home, menu: false }
 ];
